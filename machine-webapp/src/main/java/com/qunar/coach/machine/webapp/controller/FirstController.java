883337b8af08167e9ee1bdb12996741ebe83fa31
zhangyunfan <zhangyunfan@bytedance.com>
2015-10-24 23:51:16 +0800

    First runnable
    Change-Id: I34bd38e468abe12dea51f726133c4ed13bbc1231

--- machine-webapp/src/main/java/com/qunar/coach/machine/webapp/controller/FirstController.java
+++ machine-webapp/src/main/java/com/qunar/coach/machine/webapp/controller/FirstController.java
@@ -2,7 +2,9 @@ package com.qunar.coach.machine.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 /**
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api")
 public class FirstController {
 
-    @RequestMapping("/v1/hello")
+    @RequestMapping(value = "/v1/hello", method = RequestMethod.GET)
+    @ResponseBody
     public String getVideoBeanList(
             @RequestParam(value = "name", required = false, defaultValue = "") String name) {
         return "Hello " + name;
