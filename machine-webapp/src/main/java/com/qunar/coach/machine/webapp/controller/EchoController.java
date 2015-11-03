3f08feccbc9d2a5ffb2caa065ab16e6306afe21c
nellyshowtime <nellyshowtime@gmai.com>
2015-11-03 11:53:05 +0800

    rename.

--- machine-webapp/src/main/java/com/qunar/coach/machine/webapp/controller/EchoController.java
+++ machine-webapp/src/main/java/com/qunar/coach/machine/webapp/controller/EchoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("/api")
-public class FirstController {
+public class EchoController {
 
     @Autowired
     private TestService testService;
