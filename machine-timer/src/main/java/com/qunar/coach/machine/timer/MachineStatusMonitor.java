561387712f691615ab48a679f3e0be23f7c12249
zhangyunfan <zhangyunfan@bytedance.com>
2015-11-05 01:32:21 +0800

    Timer
    Change-Id: I24a5c5052f0d3cde409237eedb24877a3bf1c409

---
+++ machine-timer/src/main/java/com/qunar/coach/machine/timer/MachineStatusMonitor.java
@@ -0,0 +1,28 @@
+package com.qunar.coach.machine.timer;
+
+import com.qunar.coach.machine.dao.model.tables.daos.MachineDao;
+import com.qunar.coach.machine.dao.model.tables.pojos.Machine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by zhangyunfan on 15-11-4.
+ */
+
+public class MachineStatusMonitor {
+    @Autowired
+    private MachineDao machineDao;
+    public void doTask() {
+        System.out.print("hahah");
+        List<Machine> machines = machineDao.findAll();
+        System.out.print(machines);
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:applicationContext-timer-base.xml");
+        ((ClassPathXmlApplicationContext)ctx).registerShutdownHook();
+    }
+}
